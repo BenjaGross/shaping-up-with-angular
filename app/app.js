db344a2b2e52155f3995254004aac6c0a221b2b7
Ben <benja.j.gross@gmail.com>
2016-05-06 10:31:00 -0400

    adds pannels

--- app/app.js
+++ app/app.js
@@ -10,14 +10,31 @@
     name: 'Dodecahedron',
     price: 2.95,
     description: '. . .',
+    specifications: '2swb3 carrots',
+    reviews: 'So good',
     canPurchase: true,
     soldOut: false,
+    images: [
+    {
+      full: 'dodecahedron-01-full.jpg',
+      thumb: 'dodecahedron-01-thumb.jpg'
+    }
+    ]
+
   },
   {
     name: "Pentagonal Gem",
     price: 5.95,
     description: ". . .",
+    specifications: '13brdd5 carrots',
+    reviews: 'So good',
     canPurchase: true,
     soldOut: false,
+    images: [
+    {
+      full: 'pentagonal-01-full.jpg',
+      thumb: 'pentagonal-01-thumb.jpg'
+    }
+    ]
   }]
 })();
